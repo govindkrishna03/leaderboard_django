5a8c1d52d7e30ca2d606eafb3b93998b02f76ab6
govindkrishna03 <govindkrishna28@gmail.com>
2023-10-11 21:52:16 +0530

    fixing issues

--- leaderboard_app/views.py
+++ leaderboard_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from api_github import fetch_github_commits 
 def leaderboard_view(request):
-    owner = 'Govind Krishna'
+    owner = 'govindkrishna03'
     repo = 'Hacktober_2023'
 
     commits = fetch_github_commits(owner, repo)
